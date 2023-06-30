bbdcf92fa1057916f05dfc4aa8b5ab89f99f7fcd
arandarod <rodrigo.aranda@protonmail.com>
2023-06-30 15:39:04 -0300

    Commit Inicial

---
+++ src/test/java/com/github/arandarod/springdocker/SpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arandarod.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
